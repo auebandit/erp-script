b6897934106b6715640b07ff056ccffe7eab6c91
Nolik23 <118443438+Nolik23@users.noreply.github.com>
2022-12-08 20:43:09 +0300

    Add files via upload

--- pozivnoi.lua
+++ pozivnoi.lua
@@ -1,7 +1,7 @@
 --Update: Обновил позывные/состав
 -- Информация о скрипте
 script_name('«Auto-Doklad»') 		                    -- Указываем имя скрипта
-script_version(4.38) 						            -- Указываем версию скрипта / FINAL
+script_version(4.39) 						            -- Указываем версию скрипта / FINAL
 script_author('Henrich_Rogge', 'Marshall_Milford', 'Andy_Fawkess') 	-- Указываем имя автора
 
 -- Библиотеки
@@ -25,6 +25,7 @@ local nicks = { -- [''] = '',
   
 -- Стажеры.
   ['Anthony_Murray'] = 'Муравей',
+  ['Aleksej_Mironov'] = 'Доктор',
   ['Sheogorat_Daedric'] = 'Мельник',
   ['Aitesu_Matsumoto'] = 'Скай',
   ['Adam_Rockwells'] = 'Русалка',

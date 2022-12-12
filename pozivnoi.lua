3478d870771d6c0a7e4cf71a337a10b75830190f
CEMIK13 <120196869+CEMIK13@users.noreply.github.com>
2022-12-12 22:55:20 +0300

    Add files via upload

--- pozivnoi.lua
+++ pozivnoi.lua
@@ -1,7 +1,7 @@
 --Update: Обновил позывные/состав
 -- Информация о скрипте
 script_name('«Auto-Doklad»') 		                    -- Указываем имя скрипта
-script_version(4.42) 						            -- Указываем версию скрипта / FINAL
+script_version(4.43) 						            -- Указываем версию скрипта / FINAL
 script_author('Henrich_Rogge', 'Marshall_Milford', 'Andy_Fawkess') 	-- Указываем имя автора
 
 -- Библиотеки
@@ -25,6 +25,7 @@ local nicks = { -- [''] = '',
   
 -- Стажеры.
   ['Vova_Kotov'] = 'Кот',
+  ['Waffles_Exe'] = 'Вафля',
   ['Andrew_Midnight'] = 'Полночь',
   ['Maximiliano_Stoun'] = 'Компот',
   ['Vyacheslav_Lisov'] = 'Лис',

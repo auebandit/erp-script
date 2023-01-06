c79a59284ad35cb3a3a3549a5b6d2d2e6cd5e276
Can9Cokol <41648232+Can9Cokol@users.noreply.github.com>
2023-01-06 20:12:17 +0300

    Add files via upload

--- pozivnoi.lua
+++ pozivnoi.lua
@@ -1,7 +1,7 @@
 --Update: Обновил позывные/состав
 -- Информация о скрипте
 script_name('«Auto-Doklad»') 		                    -- Указываем имя скрипта
-script_version(4.56) 						            -- Указываем версию скрипта / FINAL
+script_version(4.57) 						            -- Указываем версию скрипта / FINAL
 script_author('Henrich_Rogge', 'Marshall_Milford', 'Andy_Fawkess') 	-- Указываем имя автора
 
 -- Библиотеки
